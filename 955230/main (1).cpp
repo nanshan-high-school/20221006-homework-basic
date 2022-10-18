1a65ba049ca0fb3f5741dd575ab8b9e7f5452be8
patrick <patrickchen970625@gmail.com>
2022-10-18 23:37:41 +0800

    955230 33828 陳宣翔

---
+++ 955230/main (1).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    if (x%3==0){
+        cout << "為三的倍數";
+    }else if(x%3==1){
+        cout << "除三餘一";
+    }else {
+        cout << "除三餘二";
+}}
+    
