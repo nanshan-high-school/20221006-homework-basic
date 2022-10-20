7459ea25ca5dbfaf7f210aaf3292d4cd6793cfe2
施曼婷 <113109387+33416@users.noreply.github.com>
2022-10-20 19:22:16 +0800

    Update main (2).cpp

--- main (2).cpp
+++ main (2).cpp
@@ -5,9 +5,11 @@ int main()
     int num1;
     cout<< "what is num1\n";
     cin>> num1;
-    cout<< num1%3 ;
-
-    return 0;
-    
-    
+    if (num1％3==0){
+    cout << "為三的倍數";
+    } else if (num1％3==1){
+    cout << "除以三餘一";
+    } else if (num1％3==2){
+    cout << "除以三餘二";
+    }
 }
